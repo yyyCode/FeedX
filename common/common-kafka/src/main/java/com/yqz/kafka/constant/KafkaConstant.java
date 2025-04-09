7aad358c423d497047d2ddbd13b7255de6d9bcfa
y qz <144-yqz@users.noreply.gitlab.topviewclub.cn>
2025-04-09 21:41:48 +0800

    feat:编写common-kafka模块

--- common/common-kafka/src/main/java/com/yqz/kafka/constant/KafkaConstant.java
+++ common/common-kafka/src/main/java/com/yqz/kafka/constant/KafkaConstant.java
@@ -1,4 +1,8 @@
 package com.yqz.kafka.constant;
-
 public class KafkaConstant {
+
+    public static final String VOUCHER_KILL_ORDER_TOPIC = "voucher_kill_order_topic";
+
+    public static final String DEFAULT_GROUP = "default-group";
 }
+
