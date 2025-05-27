17d7d603ecdc63581b3cd043cb32d7b75f3db532
y qz <2678785492@qq.com>
2025-05-27 14:58:57 +0800

    feat：完成countService方法的实现

--- business/count/src/main/java/com/yqz/count/service/impl/CountServiceImpl.java
+++ business/count/src/main/java/com/yqz/count/service/impl/CountServiceImpl.java
@@ -32,36 +32,78 @@ public class CountServiceImpl implements CountService, CountServiceIRPC {
     @Resource
     private RedisTemplate redisTemplate;
 
+    @Autowired
+    @Qualifier(value = "redissonClient")
+    private RedissonClient redissonClient;
+
+    private Long itemId;
+
+    private BatchUtil<Integer, Integer> batchUtil;
+
+    @PostConstruct
+    private void postConstructorInit() {
+        // todo 当请求数量达到20个，或每过5s合并执行一次请求
+        batchUtil = BatchUtil.getInstance((input, handlerType) -> {
+            System.out.println("处理类型:" + handlerType + ",接受到批量请求参数:" + input);
+            int sum = input.stream().mapToInt(x -> x).sum();
+            if (sum != 0){
+                redisTemplate.opsForValue().increment("item:" + itemId + ":like", sum);
+            }
+            return null;
+        }, 100, 5);
+    }
+
+    @Override
+    public Integer like(LikeBo likeBo) {
+        // todo 完善请求合并工具类
+        itemId = likeBo.getItemId();
+        RBloomFilter<Object> bloomFilter = redissonClient.getBloomFilter(likeBo.getUserId() + ":like:" + "Bloom");
+        bloomFilter.tryInit(1000L,0.01);
+        if (likeBo.getType() == 1 && !bloomFilter.contains(itemId)) {
+            return 1;
+        }else if(likeBo.getType() == 0){
+            return 0;
+        }
+        // 兜底
+        return null;
+    }
+
 
     @Override
     public Long getLikeCountByItemId(Long itemId) {
-        // todo 按照itemId来查询点赞量
+        // 查询item点赞量
         Object o = redisTemplate.opsForValue().get("item:" + itemId + ":like");
         return (Long) o;
     }
 
     @Override
     public Long getCollectCountByItemId(Long itemId) {
-        return 0L;
+        // 获取item的收藏量
+       return (Long) redisTemplate.opsForValue().get("item:" + itemId + ":collect");
     }
 
     @Override
     public Long getCommentCountByItemId(Long itemId) {
-        return 0L;
+        // 获取item的评论数量
+        return (Long) redisTemplate.opsForValue().get("item:" + itemId + ":comment");
     }
 
     @Override
     public Long getViewCountByItemId(Long itemId) {
-        return 0L;
+        // 获取item的观看数量
+        return (Long) redisTemplate.opsForValue().get("item:" + itemId + ":view");
     }
 
     @Override
     public StatisticsDTO getStatisticsDTO(Long itemId) {
-        return null;
+        // 计算统计数据
+        StatisticsDTO statisticsDTO = new StatisticsDTO();
+        statisticsDTO.setCollectCount(getCollectCountByItemId(itemId));
+        statisticsDTO.setCommentCount(getCommentCountByItemId(itemId));
+        statisticsDTO.setLikeCount(getLikeCountByItemId(itemId));
+        statisticsDTO.setViewCount(getViewCountByItemId(itemId));
+        return statisticsDTO;
     }
 
-    @Override
-    public Integer like(LikeBo likeBo) {
-        return 0;
-    }
+
 }
